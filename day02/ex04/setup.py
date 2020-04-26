bf777bd2e52600f4ec9d61dbc1bcb1feb15e1896
허지윤 <jiyoonhur@heojiyun-ui-MacBookPro.local>
2020-04-26 11:59:56 +0900

    day02

---
+++ day02/ex04/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                = 'ai42',
+    version             = '1.0.0',
+    description         = 'minipack',
+    author              = 'inno.aca',
+)
