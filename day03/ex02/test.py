9a1b61b75251f29494f6d0fb4b59514415a58439
허지윤 <jiyoonhur@heojiyun-ui-MacBookPro.local>
2020-04-27 20:35:23 +0900

    day03

--- day03/ex02/test.py
+++ day03/ex02/test.py
@@ -2,8 +2,9 @@ import numpy as np
 import string
 array = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5],[1, 2, 3, 4, 5]]
 n = 2
-i = 0
-#for i in range(n-1, len(array), n-1):
-array = np.concatenate((array[:i,:], array[i+1:,:]), axis=0)
+
+for i in range(n-1, len(array), n-1):
+	arr1 = array[:i,:]
+	#array = np.concatenate((array[ :i, : ], array[i+1: , : ]), axis=0)
 
 print(array)
---
+++ day03/ex03/ColorFilter.py
@@ -0,0 +1,13 @@
+class ColorFilter:
+	def invert(self, arr):
+		pass
+	def to_blue(self, arr):
+		pass
+	def to_green(self, arr):
+		pass
+	def to_red(self, arr):
+		pass
+	def celluloid(self, arr):
+		pass
+	def to_grayscale(self, arr, filter):
+		pass
