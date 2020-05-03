4a268105c25ea140ba7103d0391a9d276a542720
jiyoonhur <jiyoon1156@gmail.com>
2020-05-03 15:15:30 +0900

    day04

--- day04/ex06/MyPlotLib.py
+++ day04/ex06/MyPlotLib.py
@@ -3,12 +3,23 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 class MyPlotLib:
-
+	@staticmethod
     def histogram(data, features):
-    	pass
+    	features = [f for f in features if np.issubdtype(data[f].dtype, np.number)]
+        data[features].hist()
+        plt.show()
+	@staticmethod
     def density(data, features):
-    	pass
+    	features = [f for f in features if np.issubdtype(data[f].dtype, np.number)]
+        data[features].plot.kde(alpha=0.5)
+        plt.show()
+	@staticmethod
     def pair_plot(data, features):
-    	pass
+    	features = [f for f in features if np.issubdtype(data[f].dtype, np.number)]
+        pd.plotting.scatter_matrix(data[features], alpha=0.2)
+        plt.show()
+	@staticmethod
     def box_plot(data, features):
-		pass
+		features = [f for f in features if np.issubdtype(data[f].dtype, np.number)]
+        data[features].plot.box()
+        plt.show()
